f4f025303ffc4667465e610962d026472dfdd37c
Ryan Vennell <ryan.vennell@gmail.com>
2016-05-08 03:53:30 -0500

    Adding example code and asset

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
-func main () {
+import (
+	"flag"
+	"net/http"
+
+	"github.com/hickeroar/enliven"
+	"github.com/hickeroar/enliven/plugins"
+)
+
+func rootHandler(rw http.ResponseWriter, r *http.Request, ev enliven.Enliven) {
+	rw.Header().Set("Content-Type", "text/plain")
+	rw.Write([]byte("It's working!!"))
+}
+
+// Example/Test usage
+func main() {
+	ev := enliven.New(make(map[string]string))
+
+	// Serving static assets from the ./static/ folder as the /assets/ route
+	ev.InitPlugin(plugins.NewStaticAssetPlugin("/assets/", "./static/"))
+
+	// Simple route handler
+	ev.AddRoute("/", enliven.RouteHandlerFunc(rootHandler))
+
+	port := flag.String("port", "8000", "The port the server should listen on.")
+	flag.Parse()
+
+	ev.Run(*port)
 }
