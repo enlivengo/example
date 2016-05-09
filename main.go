dd36335d087f21c11d39b8da9aa5ef364b125ff5
Ryan Vennell <ryan.vennell@gmail.com>
2016-05-09 03:32:33 -0500

    Adding embedded asset example.

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"net/http"
+	"time"
 
 	"github.com/hickeroar/enliven"
+	_ "github.com/hickeroar/enliven-example/statik"
 	"github.com/hickeroar/enliven/plugins"
+	"github.com/jinzhu/gorm"
 )
 
 func rootHandler(rw http.ResponseWriter, r *http.Request, ev enliven.Enliven) {
@@ -13,15 +16,40 @@ func rootHandler(rw http.ResponseWriter, r *http.Request, ev enliven.Enliven) {
 	rw.Write([]byte("It's working!!"))
 }
 
+// User Is a simple user model
+type User struct {
+	gorm.Model
+
+	Birthday time.Time
+	Age      int
+	Name     string
+	Email    string `gorm:"type:varchar(100);unique_index"`
+	Password string
+}
+
 // Example/Test usage
 func main() {
-	ev := enliven.New(make(map[string]string))
+	config := map[string]string{
+		"db.driver":   "postgres",
+		"db.host":     "127.0.0.1",
+		"db.user":     "postgres",
+		"db.dbname":   "enliven",
+		"db.password": "postgres",
+	}
+
+	ev := enliven.New(config)
 
 	// Serving static assets from the ./static/ folder as the /assets/ route
 	ev.InitPlugin(plugins.NewStaticAssetPlugin("/assets/", "./static/"))
 
+	// The statik import sets up the data that will be used by the statik filesystem
+	// Example: '_ "github.com/hickeroar/enliven-example/statik"'
+	ev.InitPlugin(plugins.NewStatikAssetPlugin("/statik/"))
+
 	// Simple route handler
 	ev.AddRoute("/", enliven.RouteHandlerFunc(rootHandler))
+
+	ev.GetDatabase().AutoMigrate(&User{})
 
 	port := flag.String("port", "8000", "The port the server should listen on.")
 	flag.Parse()
